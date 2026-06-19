9ee8296a1631c44f6cf216d08edaa03b90a776ff
Athembele Dladla <atdlajhb025@student.wethinkcode.co.za>
2026-06-19 11:17:01 +0200

    feat: add output reporting and temperature alert logic

--- weather_pipeline.py
+++ weather_pipeline.py
@@ -6,12 +6,11 @@ def run_weather_pipeline(city: str, temp_threshold: float):
     print(f"--- Starting Weather Pipeline for {city.title()} ---")
 
     # extract raw JSON data from the API
-    # wttr.in gives clean JSON if appended
-    url = f"https://wttr.in/{city}?format=j1"
+    url = f"https://wttr.in/{city}?format=j1"                         # wttr.in gives clean JSON if appended
 
     try:
         response = requests.get(url)
-        response.raise_for_status()  # checks for HTTP errors
+        response.raise_for_status()                                    # checks for HTTP errors
         raw_data = response.json()
         print("[SUCCESS] Data extracted from API.")
     except Exception as e:
@@ -20,8 +19,7 @@ def run_weather_pipeline(city: str, temp_threshold: float):
     
     #transform
     try:
-        # Navigate JSON structure to get current conditions
-        current_condition = raw_data["current_condition"][0]
+        current_condition = raw_data["current_condition"][0]            # Navigates JSON structure to get current conditions
 
         temp_c = float(current_condition["temp_C"])
         humidity = int(current_condition["humidity"])
@@ -41,3 +39,20 @@ def run_weather_pipeline(city: str, temp_threshold: float):
     except KeyError as e:
         print(f"[ERROR] JSON structure changed or field missing: {e}")
         return
+    
+    print("\n--- PROCESSED DATA REPORT ---")
+    print(json.dumps(processed_data, indent=4))
+    print("-----------------------------\n")
+
+    if processed_data["is_alert_triggered"]:
+        print(
+            f"ALERT! The temperature in {processed_data['city']} is {temp_c}°C."
+        )
+        print(f"This exceeds your threshold of {temp_threshold}°C!")
+    else:
+        print(" Temperature is within normal limits. No alert triggered.")
+
+
+# Run the pipeline
+# Change 'London' to your city and '20' to your target temperature alert threshold
+run_weather_pipeline(city="Cape Town", temp_threshold=20.0)
